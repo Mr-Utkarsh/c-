72b3009eaf07f614ba013f338592993f069c31e6
Utkarsh Nageshwar <utkarshnageshwar@gmail.com>
2025-03-05 23:47:41 +0530

    first commit

---
+++ q1.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Q1 write a program to print,subtract,multiply,modulus% & division/ of two numbers;
+//    int a,b;
+//    cout<<"enter 1 no = " <<endl;
+//    cin>>a;
+//     cout<<"enter 2 no = "<<endl;
+//     cin>>b;
+//     cout<<"the ans of a+b is = "<<a+b<<endl;
+//     cout<<"the ans of a-b is = "<<a-b<<endl;
+//     cout<<"the ans of a*b is = "<<a*b<<endl;
+//     cout<<"the ans of a/b is = "<<a/b<<endl;
+//     cout<<"the ans of a%b is = "<<a%b<<endl;
+
+
+   //Q2 write a program to find area of circle;
+    //  int r;
+    //     cout<<"enter the radius of circle = "<<endl;
+    //     cin>>r;
+    //     cout<<"the area of circle is = "<<3.14*r*r<<endl;
+
+
+   //Q3 write a program to find simple interest;
+    // int p,t,r;
+    // cout<<"enter the principle amount = "<<endl;
+    // cin>>p;
+    // cout<<"enter the time = "<<endl;
+    // cin>>t;
+    // cout<<"enter the rate = "<<endl;
+    // cin>>r;
+    // cout<<"the simple interest is = "<<(p*t*r)/100<<endl;
+
+
+   //Q4 write a program to convert celsius to fahrenheit;
+    // float c;
+    // cout<<"enter the celsius = "<<endl;
+    // cin>>c;
+    // cout<<"the fahrenheit is = "<<(c*9/5)+32<<endl;
+
+
+   //Q5 write a program to find square root of a number;
+    // int a;
+    // cout<<"enter the number = "<<endl;
+    // cin>>a;
+    // cout<<"the square root of a number is = "<<a*a<<endl;
+
+
+   //Q6 write a program to find cube of a number;
+    // int a;
+    // cout<<"enter the number = "<<endl;
+    // cin>>a;
+    // cout<<"the cube of a number is = "<<a*a*a<<endl;
+
+    //Control flow statement:- it includes 3 types of statements:-
+    //1 conditional statement-- there are 4 types:- single,BI,Ladder,nested
+    //2 transfer statement--there are 3 types:- Break,continue,switch
+    //3 looping statement-there are 3 types:- do while,while,for
+
+    //1 conditional statement :-
+    //1 single conditional statement :- singal candition will be executed if and only if the condition is true otherwise it will not be executed
+    // syntax:- if(condition/expression)
+    // {
+    // statement
+    // }
+    // int age;
+    // cout<<"enter the age = "<<endl;
+    // cin>>age;
+    // if(age>=18)
+    // {
+    //     cout<<"you are eligible for voting"<<endl;
+    // }
+
+
+    //condition with expression example
+    // int a = 1;
+    // if (a,a--,a++)
+    // {
+    //     cout<<"true"<<endl;
+    // }
+
+    //2 BI conditional statement:- two conditions will be executed if and only if the condition is true otherwise it will not be executed
+    // syntax:- if(condition/expression)
+    // {
+    // statement
+    // }
+    // else
+    // {
+    // statement
+    // }
+
+    //Q7 write a program to find out the given no is even or odd;
+    // int a;
+    // cout<<"enter the number = "<<endl;
+    // cin>>a;
+    // if(a%2==0)
+    // {
+    //     cout<<"the given no is even"<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"the given no is odd"<<endl;
+    // }    
+
+    //Q8 write a program to find out the large no between 2 integers;
+    // int a,b;
+    // cout<<"enter 1 no = "<<endl;
+    // cin>>a;
+    // cout<<"enter 2 no = "<<endl;
+    // cin>>b;
+    // if(a>b)
+    // {
+    //     cout<<"the large no is = "<<a<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"the large no is = "<<b<<endl;
+    // }
+
+    //Q9 write a program to find out the given character is vowel or consonant;
+    // char ch;
+    // cout<<"enter the character = "<<endl;
+    // cin>>ch;
+    // if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U' )
+    // {
+    //     cout<<"the given character is vowel"<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"the given character is consonant"<<endl;
+    // }
+
+    //Q10   4 digit password program
+//     int pw,otp;
+//     int opw = 2025;
+//    int ootp = 1000;
+//    cout<<"create password = ";
+//    cin>>pw;
+   
+//     if (pw>=1000 && pw<=9999)
+//     {
+       
+//       if (pw==opw)
+//         {
+//             cout<<"enter the otp = ";
+//             cin>>otp;
+//             if (otp==ootp)
+//             {
+//                cout<<"welcome";
+//             }
+//             else
+//             {
+//                 cout<<"invalid otp";
+//             }
+            
+//         }
+//         else
+//         {
+//             cout<<"wrong password";
+//         }  
+//     }
+//     else
+//     {
+//         cout<<"invalid";
+//     }
+}
